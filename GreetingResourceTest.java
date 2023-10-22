6bc6b4609d0dbf9093c153e978370770b9605c7c
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2023-10-22 02:38:08 +0200

    Update GreetingResourceTest.java

--- GreetingResourceTest.java
+++ GreetingResourceTest.java
@@ -11,12 +11,12 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class GreetingResourceTest {
 
-    @Test    
+    @Test
     public void testHelloEndpoint() {
         given()
           .when().get("/hello")
           .then()
-             .statusCode(200)    
+             .statusCode(200)
              .body(is("Hello Quarkus"));
     }
 
