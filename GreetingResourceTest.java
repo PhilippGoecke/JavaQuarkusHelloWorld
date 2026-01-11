4827727d06032087f8ef8b91697c425f9fa8f075
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-11 21:21:19 +0100

    Update GreetingResourceTest to use startsWith matcher

--- GreetingResourceTest.java
+++ GreetingResourceTest.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
 
 @QuarkusTest
 public class GreetingResourceTest {
@@ -17,18 +18,17 @@ public class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello World!"));
+             .body(startsWith("Hello World!"));
     }
 
     @Test
     public void testGreetingEndpoint() {
         String uuid = UUID.randomUUID().toString();
         given()
-          .pathParam("name", uuid)
           .when().get("/hello/greeting/{name}")
           .then()
             .statusCode(200)
-            .body(is("Hello " + uuid + "!"));
+            .body(startsWith("Hello " + uuid + "!"));
     }
 
 }
