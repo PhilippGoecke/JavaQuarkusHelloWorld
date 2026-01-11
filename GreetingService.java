5689dab8aacdb43c1312ea27398f189a0c2f8346
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-11 21:12:35 +0100

    Update greeting method to use Quarkus version

--- GreetingService.java
+++ GreetingService.java
@@ -1,13 +1,10 @@
 package org.acme;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import io.quarkus.runtime.Version;
 
 @ApplicationScoped
 public class GreetingService {
-
     public String greeting(String name) {
-        return "Hello " + name + "!<br>(Quarkus " + Version.getVersion() + ")";
+        return "Hello " + name + "! (Quarkus " + io.quarkus.runtime.Quarkus.class.getPackage().getImplementationVersion() + ")";
     }
-
 }
