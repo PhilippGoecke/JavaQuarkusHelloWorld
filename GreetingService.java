98d180413c9bc419d527d6e412ed010682932b8c
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-11 20:58:30 +0100

    Update GreetingService.java

--- GreetingService.java
+++ GreetingService.java
@@ -1,12 +1,13 @@
 package org.acme;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import io.quarkus.runtime.Version;
 
 @ApplicationScoped
 public class GreetingService {
-
     public String greeting(String name) {
-        return "Hello " + name + "!<br>(Quarkus " + io.quarkus.runtime.Version.getVersion() + ")";
+        return "Hello " + name + "!<br>(Quarkus " + Version.getVersion() + ")";
+    }
     }
 
 }
