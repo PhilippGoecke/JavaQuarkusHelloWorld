4910a3fee5e78fb9cf80032fe4e1488643389a4d
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-11 22:07:15 +0100

    Update greeting method to include HTML line break

--- GreetingService.java
+++ GreetingService.java
@@ -5,6 +5,6 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class GreetingService {
     public String greeting(String name) {
-        return "Hello " + name + "! (Quarkus " + io.quarkus.runtime.Quarkus.class.getPackage().getImplementationVersion() + ")";
+        return "Hello " + name + "!<br>(Quarkus " + io.quarkus.runtime.Quarkus.class.getPackage().getImplementationVersion() + ")";
     }
 }
